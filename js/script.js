516b6345915f3e772df17c047a39e9e36b0a0784
oldmanstreetcoding <furqan.right.here@gmail.com>
2020-10-03 22:30:40 +0700

    persiapan menambahkan halaman profil dan layanan

--- js/script.js
+++ js/script.js
@@ -3,28 +3,52 @@
 /* eslint-disable no-undef */
 
 $(document).ready(() => {
+    // start load menu
     $.get('../pages/component/nav.html', (data) => {
         $('.topnav, .sidenav').html(data);
     });
 
     $('.sidenav').sidenav();
 
+    // end load menu
+
+    // start load detil profil
+    const loadprofil = (detil) => {
+        $.get(`../pages/profil/${detil}.html`, (data, status) => {
+            if (status === 'success') {
+                $('#boxprofil').html(data);
+            }
+        });
+    };
+
+    $(document).on('click', '.tabprofil', function () {
+        const detil = $(this).attr('id');
+        loadprofil(detil);
+    });
+
+    // end load detil profil
+
+    // start load page
     const loadpage = (page) => {
         $.get(`../pages/${page}.html`, (data, status) => {
             if (status === 'success') {
                 $('#body-content').html(data);
 
-                const h = window.innerHeight;
+                if (page === 'beranda') {
+                    const h = window.innerHeight;
 
-                $('.slider').slider({
-                    indicators: false,
-                    height: h - 118,
-                });
+                    $('.slider').slider({
+                        indicators: false,
+                        height: h - 118,
+                    });
+                }
+
+                if (page === 'profil') {
+                    loadprofil('sambutan');
+                }
             }
         });
     };
-
-    loadpage('beranda');
 
     $(document).on('click', '.btn-nav', function () {
         $('.btn-nav').css('text-decoration', 'none');
@@ -35,6 +59,11 @@ $(document).ready(() => {
         loadpage(page);
     });
 
+    loadpage('beranda');
+
+    // end load page
+
+    // start load form login
     $(document).on('click', '#showlogin', () => {
         $.get('../pages/component/boxlogin.html', (data) => {
             $('#boxlogin').html(data);
@@ -42,4 +71,6 @@ $(document).ready(() => {
     });
 
     $('.modal').modal();
+
+    // end load form login
 });
